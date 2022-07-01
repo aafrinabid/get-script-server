e75a13fe3141d7f1144bdf76bfce187f2ce7cb01
aafrinabid <mohdaafrin@gmail.com>
2022-07-01 16:39:42 +0530

    scripts table going create designed

--- init.sql
+++ init.sql
@@ -35,7 +35,7 @@ CREATE TABLE scriptwriter (
 );
 
 CREATE TABLE producerDetails (
-    producer_id VARCHAR NOT NULL,
+    producer_id uuid,
     companyName VARCHAR NOT NULL,
     experience VARCHAR NOT NULL,
     noOfProduced VARCHAR NOT NULL,
@@ -43,5 +43,61 @@ CREATE TABLE producerDetails (
     country VARCHAR NOT NULL,
     FOREIGN KEY (producer_id),
     REFRENCES producers(producer_id)
+);
+
+CREATE TABLE scripts(
+    scriptwriter_id uuid,
+    script_id uuid DEFAULT uuid_generate_v4 (),
+    is_deleted BOOLEAN NOT NULL,
+    PRIMARY KEY (script_id)
+    FOREIGN KEY (scriptwriter_id)
+    REFRENCES scriptwriter(scriptwriter_id)
+
+);
+
+CREATE TABLE script_detail(
+    script_id uuid,
+    script_title VARCHAR NOT NULL,
+    entertainment VARCHAR NOT NULL,
+    script_type VARCHAR NOT NULL,
+    genres VARCHAR NOT NULL,
+    languages VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    is_deleted BOOLEAN NOT NULL,
+    FOREIGN KEY (script_id)
+    REFRENCES scripts(script_id)
+
+
+
+);
+
+CREATE TABLE script_pitch(
+    script_id uuid,
+    the_origin VARCHAR NOT NULL,
+    human_hook VARCHAR NOT NULL,
+    desires VARCHAR NOT NULL,
+   obstacles VARCHAR NOT NULL,
+    highlights VARCHAR NOT NULL,
+   open_road VARCHAR NOT NULL,
+   is_deleted BOOLEAN NOT NULL,
+   FOREIGN KEY (script_id)
+   REFRENCES scripts(script_id)
+
+
+
+);
+
+CREATE TABLE script_media(
+    script_id uuid,
+    script_pdf_url VARCHAR NOT NULL,
+    script_poster VARCHAR NOT NULL,
+    script_mini_poster VARCHAR NOT NULL,
+    script_video VARCHAR NOT NULL,  
+    is_deleted BOOLEAN NOT NULL,
+    FOREIGN KEY (script_id)
+    REFRENCES scripts(script_id)
+ 
+
+
 );
 
